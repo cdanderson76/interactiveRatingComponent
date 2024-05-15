7743e0201091290cfbff296106609612cd2905be
Clifford Anderson <c.andersoninfotech@gmail.com>
2024-05-15 15:15:44 -0500

    finished the "thank you" boilerplate component and also added an animated svg

--- script.js
+++ script.js
@@ -17,15 +17,16 @@ subBtn.addEventListener('click', function() {
                 <img src=${paymentImg} class="payment-img" />
 
                 <div class="rating-result-container">
-
+                    You selected 4 out of 5
                 </div>
 
                 <div class="thanks">
-
+                    Thank you!
                 </div>
 
                 <div class="para-conf">
-
+                    We appreciate you taking the time to give a rating.  If 
+                    you ever need more support, don't hesitate to get in touch!
                 </div>
 
             </main>`
